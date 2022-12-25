323e689d2ca597f0db2098a1636c6e70f7455991
Abhishek Gupta <abhishekgupta1992@outlook.com>
2022-12-25 17:20:36 +0530

    changes related to query-parameter

--- src/hello.clj
+++ src/hello.clj
@@ -2,19 +2,51 @@
   (:require [io.pedestal.http :as http]          
             [io.pedestal.http.route :as route]))
 
+(def unmentionables #{"YHWH" "Voldemort" "Mxyzptlk" "Rumplestiltskin" "曹操"})
 
-(defn respond-hello [request]          
-  {:status 200 :body "Hello, world!"})
+(defn ok [body]
+  {:status 200 :body body})
+
+(defn not-found []
+  {:status 404 :body "Not found\n"})
+
+(defn greeting-for [nm]
+  (cond
+    (unmentionables nm) nil
+    (empty? nm)         "Hello, world!\n"
+    :else               (str "Hello, " nm "\n")))
+
+(defn respond-hello [request]
+  (let [nm   (get-in request [:query-params :name])
+        resp (greeting-for nm)]
+    (if resp
+      (ok resp)
+      (not-found))))
 
 (def routes
   (route/expand-routes
     #{["/greet" :get respond-hello :route-name :greet]}))
 
-(defn create-server []
-  (http/create-server
-    {::http/routes routes
-     ::http/type   :jetty
-     ::http/port   8890}))
+(def service-map
+  {::http/routes routes
+   ::http/type   :jetty
+   ::http/port   8890})
 
 (defn start []
-  (http/start (create-server)))
+  (http/start (http/create-server service-map)))
+
+;; For interactive development
+(defonce server (atom nil))
+
+(defn start-dev []
+  (reset! server
+          (http/start (http/create-server
+                        (assoc service-map
+                          ::http/join? false)))))
+
+(defn stop-dev []
+  (http/stop @server))
+
+(defn restart []
+  (stop-dev)
+  (start-dev))
