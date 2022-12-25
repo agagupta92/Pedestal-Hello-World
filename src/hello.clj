232fd33c0ad2637ce235c66fb5ff79631e191b48
Abhishek Gupta <abhishekgupta1992@outlook.com>
2022-12-25 17:01:00 +0530

    added route ,create server & start methods

--- src/hello.clj
+++ src/hello.clj
@@ -5,3 +5,16 @@
 
 (defn respond-hello [request]          
   {:status 200 :body "Hello, world!"})
+
+(def routes
+  (route/expand-routes
+    #{["/greet" :get respond-hello :route-name :greet]}))
+
+(defn create-server []
+  (http/create-server
+    {::http/routes routes
+     ::http/type   :jetty
+     ::http/port   8890}))
+
+(defn start []
+  (http/start (create-server)))
